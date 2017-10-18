64320b8981ca54af3775f5692b1c0b3d35a6fc8d
Grumbo <blackwolfsden@hotmail.com>
2017-10-17 21:00:57 -0600

    Update VIP_pvp_gold_reward.lua

--- VIP_pvp_gold_reward.lua
+++ VIP_pvp_gold_reward.lua
@@ -1,6 +1,8 @@
 function Pvp_Gold_Reward(_, killer, killed)
 local Paccid = killer:GetAccountId()
 
+	if not(ACCT[Paccid]) then Player_Vip_Table(0, player) end;
+
 killer:ModifyMoney(killed:GetCoinage()*(0.05 * ACCT[Paccid].Vip)) -- reward is 0.05 = 5% of victims gold multiplied by killers VIP level
 
 end
