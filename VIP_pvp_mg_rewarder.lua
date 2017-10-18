209a8f2a1a6fafa25ca7154611e91b01b4352d5e
Grumbo <blackwolfsden@hotmail.com>
2017-10-17 21:01:59 -0600

    Update VIP_pvp_mg_rewarder.lua

--- VIP_pvp_mg_rewarder.lua
+++ VIP_pvp_mg_rewarder.lua
@@ -4,12 +4,12 @@
 -- PVP reward system. player gets awarded 3 mg per kill plus a bonus mg count equal to the vip level (1-VIPMAX) of the victim ROUGH DRAFT
 
 function GrantPvPReward(event, killer, killed)
+
 	if (killer:GetTeam())~=(killed:GetTeam()) then
+	
         UpdatePvpReward(killer, killed)
-			if (killer:GetMapId()==489) then
-				killer:AddItem(20558, math.random(1, 3))
-			else
-			end 
+        
+			if (killer:GetMapId()==489) then killer:AddItem(20558, math.random(1, 3)) end;
 	end
 end
  
