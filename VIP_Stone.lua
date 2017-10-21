25478f128af3dc246b2414a6e8d9a7c4389d77e0
Grumbo <blackwolfsden@hotmail.com>
2017-10-20 20:05:44 -0600

    Update VIP_Stone.lua

--- VIP_Stone.lua
+++ VIP_Stone.lua
@@ -4,8 +4,6 @@ local itemid = ACCT["SERVER"].Vip_stone
 local function RemoveVIPstone(event, _, _, player)
 local Paccid = player:GetAccountId()
 
-	if not(ACCT[Paccid]) then Player_Vip_Table(0, player) end;
-
 	SetVip(player, ACCT[Paccid].Vip+1)
 	UpdateVotes(player, ACCT["SERVER"].Vote_count+1) -- just to balance the math for the checker.
 	player:RemoveItem(ACCT["SERVER"].Vip_stone, 1)
